cbafd1acb4532f2a24b7249f1e2e8a1b55d42700
Nightgg42 <nightxshot29@gmail.com>
2021-03-25 23:49:08 +0700

    new flie

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+final themeColor = Color(0xFFB4CBE3);
+final primaryColor = Color(0xff203152);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
+final textColor = Color(0xFF00061C);
